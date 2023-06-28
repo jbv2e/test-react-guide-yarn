72286208da82386d5e5cfd5888ac21da7cc157d6
jbv2e <j4petv2@gmail.com>
2023-06-28 17:57:52 +0900

    80. child to parent

--- src/components/NewExpense/ExpenseForm.tsx
+++ src/components/NewExpense/ExpenseForm.tsx
@@ -1,30 +1,109 @@
 import { useState } from 'react'
 import './ExpenseForm.css'
+// import { Styled } from 'styled-components/dist/constructors/constructWithOptions'
 
-const ExpenseForm = () => {
-  const titleChangeHandler = (e) => {
-    // console.log(e.target.value)
-    setTitle(e.target.value)
+interface IExpenseFormProps {
+  onSaveExpanseData: (enterExpenseData: {
+    title: string
+    amount: string
+    date: Date
+  }) => void
+}
+// const ExpenseForm = (props: IExpenseFormProps) => {
+const ExpenseForm = <T,>(props: T) => {
+  const [enterTitle, setEnterTitle] = useState('')
+  const [enterAmount, setEnterAmount] = useState('0')
+  const [enterDate, setEnterDate] = useState('')
+
+  // const [userInput, setUserInput] = useState({
+  //   enterTitle: '',
+  //   enterAmount: '',
+  //   enterDate: '',
+  // })
+  // (property) React.InputHTMLAttributes<HTMLInputElement>.onChange?: React.ChangeEventHandler<HTMLInputElement> | undefined
+
+  // React.ChangeEvent<HTMLInputElement>
+  // const titleChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   setUserInput({
+  //     ...userInput,
+  //     enterTitle: e.target.value,
+  //   })
+  // }
+
+  // const amountChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   setUserInput({
+  //     ...userInput,
+  //     enterAmount: e.target.value,
+  //   })
+  // }
+
+  // const dateChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   setUserInput({
+  //     ...userInput,
+  //     enterDate: e.target.value,
+  //   })
+  // }
+
+  const titleChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEnterTitle(e.target.value)
   }
-  const [title, setTitle] = useState('')
-  // const [title, setTitle] = usestate
-  const [first, setfirst] = useState(second)
-  const [first, setfirst] = useState(second)
+
+  const amountChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEnterAmount(e.target.value)
+  }
+
+  const dateChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEnterDate(e.target.value)
+  }
+
+  const submitHander = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    const expenseData = {
+      title: enterTitle,
+      amount: enterAmount,
+      date: new Date(enterDate),
+    }
+
+    props.onSaveExpanseData(expenseData)
+
+    // console.log(expenseData)
+
+    setEnterTitle('')
+    setEnterAmount('')
+    setEnterDate('')
+  }
 
   return (
-    <form>
+    <form onSubmit={submitHander}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler}></input>
+          <input
+            type="text"
+            value={enterTitle}
+            onChange={titleChangeHandler}
+          ></input>
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01"></input>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={enterAmount}
+            onChange={amountChangeHandler}
+          ></input>
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2023-01-01" max="2023-12-31"></input>
+          <input
+            type="date"
+            min="2023-01-01"
+            max="2023-12-31"
+            value={enterDate}
+            onChange={dateChangeHandler}
+          ></input>
         </div>
       </div>
       <div className="new-expense__actions">
--- src/components/NewExpense/NewExpense.tsx
+++ src/components/NewExpense/NewExpense.tsx
@@ -2,9 +2,23 @@ import './NewExpense.css'
 import ExpenseForm from 'components/NewExpense/ExpenseForm'
 
 const NewExpense = () => {
+  // const saveExpenseDataHandler = (enterExpenseData: {
+  //   title: string
+  //   amount: string
+  //   date: Date
+  // }) => {
+  const saveExpenseDataHandler = <T,>(enterExpenseData: T) => {
+    const expenseData = {
+      ...enterExpenseData,
+      id: Math.random().toString(),
+    }
+
+    console.log(expenseData)
+  }
+
   return (
     <div className="new-expense">
-      <ExpenseForm></ExpenseForm>
+      <ExpenseForm onSaveExpanseData={saveExpenseDataHandler}></ExpenseForm>
     </div>
   )
 }
